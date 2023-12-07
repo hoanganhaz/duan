445fbbf3fd3e6da006001914175e99f590dbde01
hoanganhaz <145744414+hoanganhaz@users.noreply.github.com>
2023-12-07 17:48:44 +0700

    Add files via upload

---
+++ Client/layout/footer.php
@@ -0,0 +1,73 @@
+<footer>
+    <div class="footer">
+        <div class="boxcenter">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="img_footer">
+                        <img src="./img/logo.jpg" alt="">
+                    </div>
+                    <div class="diachi">
+                        <ul>
+                            <li>
+                                <i class="bi bi-geo-alt-fill"></i> 120 trịnh văn bô , phương canh , ha noi
+                            </li>
+                            <li>
+                                <i class="bi bi-telephone-fill"></i> 012345678
+                            </li>
+                            <li>
+                                <i class="bi bi-youtube"></i>
+                            </li>
+                            <li>
+                                <i class="bi bi-instagram"></i>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="text_footer">
+                        <h3>
+                            hướng dẫn
+                        </h3>
+                        <ul>
+                            <li>HƯỚNG DẪN MUA HÀNG</li>
+                            <li>GIAO NHẬN VÀ THANH TOÁN</li>
+                            <li>ĐỔI TRẢ VÀ BẢO HÀNH</li>
+                            <li>ĐĂNG KÍ THÀNH VIÊN</li>
+                            <li>TRA CỨU ĐƠN HÀNG</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="text_footer">
+                        <h3>
+                            hướng dẫn
+                        </h3>
+                        <ul>
+                            <li>HƯỚNG DẪN MUA HÀNG</li>
+                            <li>GIAO NHẬN VÀ THANH TOÁN</li>
+                            <li>ĐỔI TRẢ VÀ BẢO HÀNH</li>
+                            <li>ĐĂNG KÍ THÀNH VIÊN</li>
+                            <li>TRA CỨU ĐƠN HÀNG</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="text_footer">
+                        <h3>
+                            hướng dẫn
+                        </h3>
+                        <ul>
+                            <li>HƯỚNG DẪN MUA HÀNG</li>
+                            <li>GIAO NHẬN VÀ THANH TOÁN</li>
+                            <li>ĐỔI TRẢ VÀ BẢO HÀNH</li>
+                            <li>ĐĂNG KÍ THÀNH VIÊN</li>
+                            <li>TRA CỨU ĐƠN HÀNG</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
